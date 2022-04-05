1dd3c520d21d1236325ca4ec366728d2cb04716f
Allen Keettikkal (alk423) <alk423@mail.usask.ca>
2022-04-04 22:32:33 -0600

    Started the Prototype to my Adventure Game

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+answer = input("Would you like to play? (yes/no)")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if answer.lower().strip() == "yes":
+    answer = input("You reach a crossroad. Would you like to go left or right? (left/right)").lower().strip()
+    if answer == "left":
+        answer = input("You encounter a bandit! What will you do? (attack/run)")
+        if answer == "attack":
+            print("You attacked! Did 5 damage to the bandit! The Bandit fell into the river!")
+            print("Game Over! (Best Ending!)")
+        elif answer == "run":
+            print("You ran away from the bandit!")
+            print("Game Over! (Neutral Ending!)")
+        else:
+            print("Uh oh! That was a bad decision! Bandit attacks you! Does 25 Damage! You fainted!")
+            print("Game Over! (Worst Ending!!!)")
+    elif answer == "right":
+        print("You end up running into the vast woods! Will you survive?")
+        print("To be continued!!!")
+    else:
+        print("Invalid Choice! Game Over!")
+else:
+    print("That's too bad!")
